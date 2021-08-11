7e7b0d98fdf857646c254d550ea791f9a11c4cd7
Albert.Kamalov <Albert.Kamalov@x5.ru>
2021-08-11 23:43:48 +0300

    change used method of TreeMap

--- L04-generics/src/main/java/homework/CustomerService.java
+++ L04-generics/src/main/java/homework/CustomerService.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class CustomerService {
-    TreeMap<Customer, String> sortCustomerMap = new TreeMap<>(Comparator.comparingLong(o -> o.getScores()));
+    private TreeMap<Customer, String> sortCustomerMap = new TreeMap<>(Comparator.comparingLong(o -> o.getScores()));
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
@@ -19,14 +19,7 @@ public class CustomerService {
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        Map.Entry<Customer, String> element = sortCustomerMap.ceilingEntry(customer);
-        //у нас условие что должно быть строго больше, поэтому если такого же нашли то прибавляем единицу и ищем еще раз.
-        if (element != null && element.getKey().getScores() == customer.getScores()) {
-            Customer customerCopy = customer.clone();
-            customerCopy.setScores(customer.getScores() + 1l);
-
-            element = sortCustomerMap.ceilingEntry(customerCopy);
-        }
+        Map.Entry<Customer, String> element = sortCustomerMap.higherEntry(customer);
         return element == null ? null : new java.util.AbstractMap.SimpleEntry<>(element.getKey().clone(), element.getValue());
     }
 
